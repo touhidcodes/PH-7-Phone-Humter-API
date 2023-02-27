a134fbd872983126f736ae3d9ac664844a77825d
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-28 00:36:00 +0600

    display-phone

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-	const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhone = async (phone) => {
+	const url = `https://openapi.programming-hero.com/api/phones?search=${phone}`;
 	const res = await fetch(url);
 	const data = await res.json();
 	displayPhone(data.data);
@@ -7,13 +7,14 @@ const loadPhone = async () => {
 
 const displayPhone = (phones) => {
 	const phonesContainer = document.getElementById("phones-container");
+	phonesContainer.innerHTML = "";
 
 	phones.forEach((phone) => {
 		console.log(phone);
 		const phoneCard = document.createElement("div");
 		phoneCard.classList.add("col");
 		phoneCard.innerHTML = `
-        <div class="card h-100">
+        <div class="card h-100 p-4">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
@@ -27,4 +28,9 @@ const displayPhone = (phones) => {
 	});
 };
 
-loadPhone();
+loadPhone("iphone");
+
+const searchPhone = () => {
+	const search = document.getElementById("input-phone").value;
+	loadPhone(search);
+};
